a4d8697d81e4f133aad8bddd3485b7a092d6a21f
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-10-22 20:35:15 +0600

    Initial commit

---
+++ src/main/java/com/epam/adk/task2/text_processing/main/TextProcessingApp.java
@@ -0,0 +1,38 @@
+package com.epam.adk.task2.text_processing.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Kaikenov Adilhan on 22.10.2016.
+ *
+ * The class with main method.
+ *
+ * @author Kaikenov Adilhan.
+ */
+public class TextProcessingApp {
+
+    private static final Logger log = LoggerFactory.getLogger(TextProcessingApp.class);
+
+    /**
+     * Application starting point.
+     *
+     * @param args input arguments array.
+     */
+    public static void main(String[] args) {
+/*
+
+        TextReader reader = new TextReader(pathParam, encoding);
+        ... text = reader.read();
+
+        Preparer preparer = new Preparer();
+        ... preparedText = preparer.prepareText(text);
+
+        Parser parser = new Parser();
+        Text parse = parser.parse(preparedText);
+
+        TaskExecutor ...
+
+ */
+    }
+}

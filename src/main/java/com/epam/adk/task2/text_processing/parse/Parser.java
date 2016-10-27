a75dac8ac2c645caa7721845053e372542347fb5
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-10-27 18:14:39 +0600

    Add JavaDoc Parser.java

--- src/main/java/com/epam/adk/task2/text_processing/parse/Parser.java
+++ src/main/java/com/epam/adk/task2/text_processing/parse/Parser.java
@@ -11,8 +11,24 @@ import com.epam.adk.task2.text_processing.exception.ParsingException;
  */
 public interface Parser {
 
+    /**
+     * The method for parsing source string.
+     *
+     * @param source target source string to parse.
+     * @return Text instance.
+     * @throws ParsingException
+     */
     Text parse(String source) throws ParsingException;
 
+    /**
+     * The method for parsing source string.
+     *
+     * @param source target source string to parse.
+     * @param compositeClass Composite type class which we need to parse.
+     * @param <T> type parameterization.
+     * @return <T extends Composite> T instance.
+     * @throws ParsingException
+     */
     <T extends Composite> T parse(String source, Class<T> compositeClass) throws ParsingException;
 
 

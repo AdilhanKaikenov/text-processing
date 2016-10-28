1477005fb5f51949a88d18de5853442952fbb5ec
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-10-28 09:46:38 +0600

    Update Text.java

--- src/main/java/com/epam/adk/task2/text_processing/entity/Text.java
+++ src/main/java/com/epam/adk/task2/text_processing/entity/Text.java
@@ -1,5 +1,8 @@
 package com.epam.adk.task2.text_processing.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Text class created on 23.10.2016.
  *
@@ -8,4 +11,42 @@ package com.epam.adk.task2.text_processing.entity;
  */
 public class Text extends AbstractComposite<Paragraph> {
 
+    /**
+     * The method returns all the paragraphs of the text.
+     *
+     * @return List<Paragraph> paragraphsList.
+     */
+    public List<Paragraph> getParagraphs() {
+        return super.getComponents();
+    }
+
+    /**
+     * The method returns all the sentences of the text.
+     *
+     * @return List<Sentence> sentencesList
+     */
+    public List<Sentence> getSentences(){
+        List<Sentence> sentences = new ArrayList<>();
+        for (Paragraph paragraph : this.getParagraphs()){
+            sentences.addAll(paragraph.getComponents());
+        }
+        return sentences;
+    }
+
+    /**
+     * The method returns all the sentences of the text.
+     *
+     * @return List<Word> wordsList.
+     */
+    public List<Word> getWords(){
+        List<Word> words = new ArrayList<>();
+        for (Sentence sentence : this.getSentences()){
+            for (Component component : sentence.getComponents()){
+                if (component != null && component.getClass() == Word.class){
+                    words.add((Word) component);
+                }
+            }
+        }
+        return words;
+    }
 }
